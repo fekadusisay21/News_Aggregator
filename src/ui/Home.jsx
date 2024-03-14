95f8f15ea270efc75f7ef5cd047a6b0303bbe788
Biruk Nigusie <120112094+BirukNigusie@users.noreply.github.com>
2024-03-14 14:55:13 -0700

    news card

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,5 +1,32 @@
+/* eslint-disable react/prop-types */
+import styled from "styled-components";
+import NewsCard from "./NewsCard";
+const Styled_NewsCard = styled.div`
+display: flex;
+align-items: center;
+gap: 1%;
+width:1200px;
+`;
 function Home() {
-  return <div>Home</div>;
+  return <Styled_NewsCard><NewsCard NewsImg="https://picsum.photos/200/300"
+               NewsTitle="Title 1"
+               NewsDescription="Discription 1"
+              />
+              <NewsCard NewsImg="https://picsum.photos/200/300"
+               NewsTitle="Title 1"
+               NewsDescription="Discription 1"
+              />
+              <NewsCard NewsImg="https://picsum.photos/200/300"
+               NewsTitle="Title 1"
+               NewsDescription="Discription 1"
+              />
+              <NewsCard NewsImg="https://picsum.photos/200/300"
+               NewsTitle="Title 1"
+               NewsDescription="Discription 1"
+              /><NewsCard NewsImg="https://picsum.photos/200/300"
+              NewsTitle="Title 1"
+              NewsDescription="Discription 1"
+             /></Styled_NewsCard>;
 }
 
 export default Home;
