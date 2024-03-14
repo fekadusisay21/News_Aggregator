1e2b0aed9c3bf6f393f12663b0c800b78d2d8204
fekadusisay21 <132390088+fekadusisay21@users.noreply.github.com>
2024-03-15 01:38:41 +0300

    News Modification

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -2,31 +2,22 @@
 import styled from "styled-components";
 import NewsCard from "./NewsCard";
 const Styled_NewsCard = styled.div`
-display: flex;
-align-items: center;
-gap: 1%;
-width:1200px;
+  display: flex;
+  align-items: center;
+  gap: 1%;
+  width: 1200px;
 `;
 function Home() {
-  return <Styled_NewsCard><NewsCard NewsImg="https://picsum.photos/200/300"
-               NewsTitle="Title 1"
-               NewsDescription="Discription 1"
-              />
-              <NewsCard NewsImg="https://picsum.photos/200/300"
-               NewsTitle="Title 1"
-               NewsDescription="Discription 1"
-              />
-              <NewsCard NewsImg="https://picsum.photos/200/300"
-               NewsTitle="Title 1"
-               NewsDescription="Discription 1"
-              />
-              <NewsCard NewsImg="https://picsum.photos/200/300"
-               NewsTitle="Title 1"
-               NewsDescription="Discription 1"
-              /><NewsCard NewsImg="https://picsum.photos/200/300"
-              NewsTitle="Title 1"
-              NewsDescription="Discription 1"
-             /></Styled_NewsCard>;
+  return (
+    <Styled_NewsCard>
+      <NewsCard
+        NewsImg="https://assets3.thrillist.com/v1/image/3175606/1200x600/scale;;webp=auto;jpeg_quality=85.jpg"
+        NewsTitle="More Americans Are Trying to Get a Second Passport Right Now"
+        NewsDescription="thrillist.com - Opheli Garcia Lawler
+        The US passport does not rank highly in this new index of the world's most powerful passports. One of the most laborious parts of international travel …"
+      />
+    </Styled_NewsCard>
+  );
 }
 
 export default Home;
--- src/ui/NewsCard.jsx
+++ src/ui/NewsCard.jsx
@@ -1,109 +1,144 @@
 /* eslint-disable react/prop-types */
-import  { useState } from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import { FaShareAlt, FaBookmark, FaHeart } from "react-icons/fa";
 import { GoShareAndroid } from "react-icons/go";
 import { FaRegBookmark, FaRegHeart } from "react-icons/fa";
 import { useDark } from "../context/DarkContext";
 
-const NewsCard = ({NewsImg, NewsTitle, NewsDescription}) => {
+const NewsCard = ({ NewsImg, NewsTitle, NewsDescription }) => {
   const [isShared, setIsShared] = useState(false);
   const [isBookmarked, setIsBookmarked] = useState(false);
   const [isLiked, setIsLiked] = useState(false);
-const {isDark} = useDark();
-const CardContainer = styled.div`
-height: 70vh;
-width: 120vh;
-  background-color: ${isDark ? "#1f1f1f" : "#d2d0d0"};
-  border-radius: 10px;
-  box-shadow: -17px 26px 15px -3px rgba(0,0,0,0.1);
-  cursor: pointer;
-  transition: transform 0.2s ease;
+  const { isDark } = useDark();
 
-  &:hover {
-    transform: scale(1.05);
-  }
-`;
-const CardImage = styled.img`
-height: 50%;
-  width: 100%;
-  border-radius: 5px;
-`;
+  const CardContainer = styled.div`
+    height: 70vh;
+    width: 120vh;
+    background-color: ${isDark ? "#171819" : "#ccc"};
+    border-radius: 10px;
+    box-shadow: -17px 26px 15px -3px rgba(0, 0, 0, 0.1);
+    cursor: pointer;
+    transition: transform 0.2s ease;
+    box-shadow: 48px 46px 22px -3px rgba(0, 0, 0, 0.1);
+    &:hover {
+      transform: scale(1.05);
+    }
+  `;
+  const CardImage = styled.img`
+    height: 50%;
+    width: 100%;
+    border-radius: 10px;
+    object-fit: cover;
+  `;
 
-const CardTitle = styled.h2`
-  font-size: 20px;
-  font-family:monospace;
-  color: ${isDark ? "#000" : "#fffefe"};
-`;
+  const CardTitle = styled.h2`
+    font-size: 20px;
+    font-family: monospace;
+    color: ${isDark ? "white" : "black"};
+  `;
 
-const CardDescription = styled.p`
-  font-size: 16px;
-  font-family: monospace;
-  color: ${isDark ? "#000" : "#fffefe"};
-`;
+  const CardDescription = styled.p`
+    font-size: 16px;
+    font-family: monospace;
+    color: ${isDark ? "white" : "black"};
+    padding: 10px 20px;
+    text-align: center;
+    overflow: hidden;
+  `;
 
-const IconContainer = styled.div`
-  display: flex;
-  gap: 50px;
-  justify-content: center;
-  align-items: center;
-  margin-top: 10px;
-  ${isDark ? "#000" : "#fffefe"}
-`;
+  const IconContainer = styled.span`
+    display: flex;
+    gap: 50px;
+    justify-content: center;
+    align-items: center;
+    margin-top: 10px;
+    span {
+      color: ${isDark ? "white" : "#3c009d"};
+    }
+  `;
 
-const MoreButton = styled.button`
-  background-color: #333;
-  color: ${isDark ? "#000" : "#fffefe"};
-  border: none;
-  padding: 8px 16px;
-  border-radius: 5px;
-  font-size: 16px;
-  font-family: monospace;
-  cursor: pointer;
-  margin-top: 10px;
-  margin-bottom: 20px;
-`;
-const Styled_btnDiv = styled.div`
-margin-top: 10px;
-display: flex;
-align-items: center;
-justify-content: center;
-`;
-const Styled_News = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-`;
+  const MoreButton = styled.button`
+    background-color: ${isDark ? "#3c009d" : "#777"};
+    color: ${isDark ? "white" : "#3c009d"};
+    border: none;
+    padding: 8px 16px;
+    border-radius: 5px;
+    font-size: 16px;
+    font-family: monospace;
+    cursor: pointer;
+    margin-top: 10px;
+    margin-bottom: 20px;
+  `;
+  const Styled_btnDiv = styled.div`
+    margin-top: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `;
+  const Styled_News = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  `;
   return (
     <CardContainer>
       <CardImage src={NewsImg} alt="News Image" />
       <Styled_News>
-      <CardTitle>{NewsTitle}</CardTitle>
-      <CardDescription>
-      {NewsDescription}
-      </CardDescription>
+        <CardTitle>{NewsTitle}</CardTitle>
+        <CardDescription>{NewsDescription}</CardDescription>
       </Styled_News>
-      
+
       <IconContainer>
         {isShared ? (
-          <FaShareAlt size={16} color="#476ac9" onClick={() => setIsShared(false)} />
+          <span>
+            {" "}
+            <FaShareAlt
+              size={16}
+              color="#476ac9"
+              onClick={() => setIsShared(false)}
+            />
+          </span>
         ) : (
-          <GoShareAndroid size={16} color="#333" onClick={() => setIsShared(true)} />
+          <span>
+            {" "}
+            <GoShareAndroid size={16} onClick={() => setIsShared(true)} />
+          </span>
         )}
         {isBookmarked ? (
-          <FaBookmark size={16} color="#47c95c" onClick={() => setIsBookmarked(false)} />
+          <span>
+            {" "}
+            <FaBookmark
+              size={16}
+              color="#47c95c"
+              onClick={() => setIsBookmarked(false)}
+            />
+          </span>
         ) : (
-          <FaRegBookmark size={16} color="#333" onClick={() => setIsBookmarked(true)} />
+          <span>
+            {" "}
+            <FaRegBookmark size={16} onClick={() => setIsBookmarked(true)} />
+          </span>
         )}
         {isLiked ? (
-          <FaHeart size={16} color="#c94747" onClick={() => setIsLiked(false)} />
+          <span>
+            <FaHeart
+              size={16}
+              color="#c94747"
+              onClick={() => setIsLiked(false)}
+            />
+          </span>
         ) : (
-          <FaRegHeart size={16} color="#333" onClick={() => setIsLiked(true)} />
+          <span>
+            <FaRegHeart size={16} onClick={() => setIsLiked(true)} />
+          </span>
         )}
       </IconContainer>
 
-      <Styled_btnDiv><MoreButton>Show More</MoreButton></Styled_btnDiv>
+      <Styled_btnDiv>
+        <MoreButton>Show More</MoreButton>
+      </Styled_btnDiv>
     </CardContainer>
   );
 };
